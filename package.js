33cf84f828556bb4b66b7834d09051d5db6ddb2b
Luca Mussi <ing.luca.mussi@gmail.com>
2014-05-11 23:02:23 +0200

    package stuff

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+	summary: "Twix.js is a Moment.js plugin for working with time ranges. Use it to manipulate, interrogate, and intelligently format a block of time."
+});
+
+Package.on_use(function(api, where) {
+	where = where || ['client', 'server'];
+	api.use("moment", where);
+	if (api.imply) {
+		api.imply("moment", where);
+	}
+	api.add_files('lib/twix/bin/twix.js', where);
+	api.add_files('lib/twix/bin/lang.js', where);
+	//api.add_files('moment-twix.js', where);
+	if (api.export) {
+		api.export('Twix');
+	}
+});
